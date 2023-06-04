a46d5366a8217f3beb20b7663bf0ca769de7cb28
Sina Kamrava <sinakamrava3118@gmail.com>
2023-06-04 14:48:45 +0330

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/**/**/*.{html,js}'],
+  purge: ['./dist/**/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
