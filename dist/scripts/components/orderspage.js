300803ab811de98fc152ddca88ae206b518a7171
Sina Kamrava <sinakamrava3118@gmail.com>
2023-06-10 10:15:00 +0330

    feat : complete cart page

--- dist/scripts/components/orderspage.js
+++ dist/scripts/components/orderspage.js
@@ -1,3 +1,10 @@
+
+import { Request } from "../services/request.js";
+import { Page } from "../utilities/page.js";
+import { Product } from "../model/product.js";
+
+const page = new Page()
+
 const navigation = document.querySelectorAll('.navigation span');
 const line = document.getElementById('line');
 const activeState = document.getElementById('active-state');
@@ -29,13 +36,29 @@ function handelNavigationEvents() {
     item.addEventListener('click', () => {
       resetNav()
       const icons = item.querySelectorAll('i');
+      const txt = item.querySelector('p').textContent.toLowerCase()
+      changePageWithNavigation(txt)
       icons.forEach(icon => {
         icon.classList.toggle('hidden');
       })
+
     })
   });
 }
 
+function changePageWithNavigation(txt) {
+  switch (txt) {
+    case 'home':
+      page.go('index');
+      break;
+      case 'card':
+        page.go('cart');
+      break;
+      case 'orders':
+        page.go('order');
+        break;
+  }
+}
 
 function initOrderState() {
   
